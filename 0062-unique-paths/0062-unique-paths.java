da2e47abf86f9bb31927b05e5bcbd29e5eea2b2f
Nishant Kumar <NISHANTRAJENDRAKUMAR@GMAIL.COM>
2024-11-19 16:24:20 +0530

    Time: 0 ms (100%), Space: 40.5 MB (28.9%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,16 @@
+class Solution{
+    int m, n; Integer[][] memo;
+    public int uniquePaths(int mx, int nx){
+        m = mx; n = nx;
+        memo = new Integer[m][n];
+
+        return f(0, 0);
+    }
+    int f(int i, int j){
+        if(i>=m||j>=n) return 0;
+        if(i==m-1&&j==n-1) return 1;
+        if(memo[i][j]!=null) return memo[i][j];
+
+        return memo[i][j] = f(i+1, j) + f(i, j+1);
+    }
+}
