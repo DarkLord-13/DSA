5bd3f6d6f5a6100d6659064d9a017777c9148103
Nishant Kumar <NISHANTRAJENDRAKUMAR@GMAIL.COM>
2024-12-23 10:10:46 +0530

    Time: 5 ms (5.44%), Space: 43.5 MB (7.22%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,40 +1,32 @@
 class Solution{
     public void nextPermutation(int[] a){
-        int n = a.length;
-        int pivot = 0; boolean largest = true;
-        for(int i=n-1; i>0; i--){
-            if(a[i-1]<a[i]){
-                pivot = i-1;
-                largest = false;
-                break;
+        int n=a.length, i=n-2;
+
+        if(n<=1) return;
+
+        while(i>=0 && a[i]>=a[i+1]) i--;
+        System.out.println(i);
+        if(i<0) i=0;
+
+        int j, minj=n, smallestLargestNumber=Integer.MAX_VALUE;
+        for(j=i+1; j<n; j++){
+            if(a[j]>a[i]){
+                if(a[j]<smallestLargestNumber){
+                    smallestLargestNumber = a[j];
+                    minj = j;
+                }
             }
-        } System.out.println(pivot);
-        if(largest){
+        } System.out.println(minj);
+        if(minj==n && i==0){
             Arrays.sort(a);
             return;
         }
+        else if(minj==n) minj=n-1;
 
-        for(int i=n-1; i>pivot; i--){
-            if(a[i]>a[pivot]){
-                int temp = a[i];
-                a[i] = a[pivot];
-                a[pivot] = temp;
-                break;
-            }
-        } print(a);
+        int temp = a[i];
+        a[i] = a[minj];
+        a[minj] = temp;
 
-        int sl = n-pivot-1; System.out.println(sl);
-        for(int i=pivot+1, j=n-1; i<=pivot + sl/2; i++){
-            int temp = a[i];
-            a[i] = a[j];
-            a[j] = temp;
-            j--;
-        }
-    }
-
-    void print(int[] arr){
-        for(int i=0; i<arr.length; i++)
-            System.out.print(arr[i]+" ");
-        System.out.println();
+        Arrays.sort(a, i+1, n);
     }
 }
