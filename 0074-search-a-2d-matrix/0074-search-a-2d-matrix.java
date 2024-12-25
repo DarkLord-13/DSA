c73310668311c68fe3c50d1de1a214fc82592ead
Nishant Kumar <NISHANTRAJENDRAKUMAR@GMAIL.COM>
2024-12-25 22:44:53 +0530

    Time: 0 ms (100%), Space: 42.3 MB (40.08%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,19 +1,32 @@
 class Solution{
     public boolean searchMatrix(int[][] a, int t){
         int m = a.length, n = a[0].length;
-        int i = 0, j = n-1;
+        int rs = 0, re = m-1, rm = (rs+re)/2;
         
-        //if(a[i][j]==t) return true;
-        while(i<m && a[i][j]<t){
-            i++;
-        }
-        while(j>=0 && i<m && a[i][j]>t){
-            j--;
-        }
-        if(i>=0 && i<m && j>=0 && j<n){
-            if(a[i][j]==t) return true;
-        }
+        while(rs<re){
+            if(a[rm][n-1]>t) re = rm-1;            
+            else if(a[rm][n-1]<t) rs = rm+1;            
+            else return true;
 
+            rm = (rs+re)/2;
+        }
+        if(a[rs][n-1]==t) return true;
+        else if(a[rs][n-1]<t) rs++;
+        
+        if(rs>=m) return false;
+
+        int[] arr = a[rs];
+        int s = 0, e = n-1, mid = (s+e)/2;
+
+        while(s<e){
+            if(arr[mid]<t) s = mid+1;            
+            else if(arr[mid]>t) e = mid-1;            
+            else return true;
+
+            mid = (s+e)/2;
+        }    
+
+        if(arr[mid]==t) return true;  
         return false;
     }
 }
