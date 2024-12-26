666b1380c65f1402b2adc928208b8a798bdf97c5
Nishant Kumar <NISHANTRAJENDRAKUMAR@GMAIL.COM>
2024-12-26 13:24:33 +0530

    Time: 0 ms (100%), Space: 40.8 MB (18.18%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -7,8 +7,8 @@ class Solution{
         return f(0, 0);
     }
     int f(int i, int j){
-        if(i>=m||j>=n) return 0;
-        if(i==m-1&&j==n-1) return 1;
+        if(i==m-1 && j==n-1) return 1;
+        if(i>=m || j>=n) return 0;
         if(memo[i][j]!=null) return memo[i][j];
 
         return memo[i][j] = f(i+1, j) + f(i, j+1);
