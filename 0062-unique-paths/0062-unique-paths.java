f28797d29f98483b2bc46e3bee1d07dad06e1cf1
Nishant Kumar <NISHANTRAJENDRAKUMAR@GMAIL.COM>
2024-12-26 13:42:15 +0530

    Time: 0 ms (100%), Space: 40.9 MB (11.51%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,16 +1,18 @@
 class Solution{
-    int m, n; Integer[][] memo;
-    public int uniquePaths(int mx, int nx){
-        m = mx; n = nx;
-        memo = new Integer[m][n];
+    public int uniquePaths(int m, int n){
+        if(m==1&&n==1) return n;
+        int[][] memo = new int[m][n];
+        
+        memo[0][0] = 1;
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                if(i==0 || j==0) memo[i][j] = 1;
+                else{
+                    memo[i][j] += (memo[i-1][j] + memo[i][j-1] + 1);
+                }
+            }
+        }
 
-        return f(0, 0);
-    }
-    int f(int i, int j){
-        if(i==m-1 && j==n-1) return 1;
-        if(i>=m || j>=n) return 0;
-        if(memo[i][j]!=null) return memo[i][j];
-
-        return memo[i][j] = f(i+1, j) + f(i, j+1);
+        return memo[m-1][n-1]+1;
     }
 }
