c63d5a2db0364157ebee94706e804ae3d12943b1
Nishant Kumar <NISHANTRAJENDRAKUMAR@GMAIL.COM>
2025-03-29 11:50:26 +0530

    Time: 9 ms (94.91%), Space: 48.8 MB (43.24%) - LeetHub

---
+++ 0744-network-delay-time/0744-network-delay-time.java
@@ -0,0 +1,71 @@
+class Solution{
+    class Pair{
+        int node, t;
+
+        public Pair(int node, int t){
+            this.node = node;
+            this.t = t;
+        }
+        @Override 
+        public String toString(){
+            return node + "-" + t;
+        }
+    }
+    public int networkDelayTime(int[][] times, int n, int src){
+        int[] sd = new int[n+1];
+        Arrays.fill(sd, Integer.MAX_VALUE);
+        sd[src] = 0;
+        sd[0] = 0;
+
+        List<List<Pair>> adj = new ArrayList<>();
+        for(int i=0; i<n+1; i++){
+            adj.add(new ArrayList<>());
+        } 
+
+        for(int[] a: times){
+            int from = a[0];
+            int to = a[1];
+            int delay = a[2];
+
+            adj.get(from).add(new Pair(to, delay));
+        } //System.out.println(adj);
+
+        PriorityQueue<Pair> q = new PriorityQueue<>((a, b)-> a.t-b.t);
+        q.add(new Pair(src, 0));
+
+        while(!q.isEmpty()){
+            Pair p = q.poll();
+            int cn = p.node;
+            int ct = p.t;
+
+            for(Pair next: adj.get(cn)){
+                int nn = next.node;
+                int nt = next.t;
+
+                if(ct+nt<sd[nn]){
+                    sd[nn] = ct+nt;
+                    q.add(new Pair(nn, sd[nn]));
+                    // print(sd);
+                }
+            }
+        }
+
+        int max = 0;
+        for(int i: sd){
+            if(i==Integer.MAX_VALUE){
+                return -1;
+            }
+            else{
+                max = Math.max(max, i);
+            }
+        }
+
+        return max;
+    }
+    void print(int[] arr){
+        for(int i: arr){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+}
