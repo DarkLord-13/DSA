215c1262cecd853810b1b43bfdd3d4136a850a47
Nishant Kumar <NISHANTRAJENDRAKUMAR@GMAIL.COM>
2025-04-12 20:27:22 +0530

    Time: 3 ms (82.62%), Space: 42.5 MB (81.79%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.java
@@ -1,49 +1,67 @@
-class Solution {
-    class Pair {
+class Solution{
+    class Pair{
         TreeNode node;
         int i, j;
 
-        public Pair(TreeNode node, int i, int j) {
+        public Pair(TreeNode node, int i, int j){
             this.node = node;
             this.i = i;
             this.j = j;
         }
     }
-
-    public List<List<Integer>> verticalTraversal(TreeNode root) {
-        TreeMap<Integer, List<Integer>> map = new TreeMap<>();
-        PriorityQueue<Pair> q = new PriorityQueue<>((a, b) -> {            
-            if (a.i != b.i)
-                return a.i - b.i; // Sort by vertical level
-            else if (a.j != b.j)
-            return a.j - b.j; // Sort by horizontal distance
-            else return a.node.val - b.node.val; // Sort by node value if both are equal
-        });
-
-        //Queue<Pair> q = new LinkedList<>();
-
-        q.add(new Pair(root, 0, 0));
-
-        while (!q.isEmpty()) {
-            Pair p = q.poll();
-            TreeNode node = p.node;
-            int cn = node.val, i = p.i, j = p.j;
-
-            if (!map.containsKey(j))
-                map.put(j, new ArrayList<>());
-            map.get(j).add(cn);
-
-            if (node.left != null)
-                q.add(new Pair(node.left, i + 1, j - 1));
-            if (node.right != null)
-                q.add(new Pair(node.right, i + 1, j + 1));
+    public List<List<Integer>> verticalTraversal(TreeNode root){
+        List<List<Integer>> ll = new LinkedList<>();
+        if(root==null){
+            return ll;
         }
 
-        System.out.println(map);
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        PriorityQueue<Pair> q = new PriorityQueue<>((a, b)-> {
+            if(a.i==b.i && a.j==b.j){
+                return a.node.val - b.node.val;
+            }
+            else if(a.i != b.i){
+                return a.i - b.i;
+            }
+            else{
+                return a.j - b.j;
+            }            
+        });
+        q.add(new Pair(root, 0, 0));
 
-        List<List<Integer>> ll = new ArrayList<>();
-        for (Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
-            ll.add(entry.getValue());
+        while(!q.isEmpty()){
+            int s = q.size();
+
+            for(int x=0; x<s; x++){
+                Pair poll = q.poll();
+                TreeNode node = poll.node;
+                int i = poll.i;
+                int j = poll.j;
+
+                if(!map.containsKey(j)){
+                    map.put(j, new ArrayList<>());
+                }
+                map.get(j).add(node.val);
+
+                if(node.left!=null){
+                    q.add(new Pair(node.left, i+1, j-1));
+                }
+                if(node.right!=null){
+                    q.add(new Pair(node.right, i+1, j+1));
+                }
+            }
+        } 
+
+        int min = 99999;
+        int max = -99999;
+        for(Map.Entry<Integer, List<Integer>> entry: map.entrySet()){
+            int key = entry.getKey();
+            min = Math.min(min, key);
+            max = Math.max(max, key);            
+        }
+
+        for(int i=min; i<=max; i++){
+            ll.add(map.get(i));
         }
 
         return ll;
