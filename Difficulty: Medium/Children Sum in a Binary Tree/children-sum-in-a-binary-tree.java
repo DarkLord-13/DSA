af52df469fcdf6f956157079dbf588847c8badc5
Nishant Kumar <NISHANTRAJENDRAKUMAR@GMAIL.COM>
2025-05-27 10:20:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Children Sum in a Binary Tree/children-sum-in-a-binary-tree.java
+++ Difficulty: Medium/Children Sum in a Binary Tree/children-sum-in-a-binary-tree.java
@@ -1,147 +1,24 @@
-//{ Driver Code Starts
-//Initial Template for Java
-
-
-
-
-//Initial Template for Java
-
-//Contributed by Sudarshan Sharma
-import java.util.LinkedList; 
-import java.util.Queue; 
-import java.io.*;
-import java.util.*;
-
-class Node{
-    int data;
-    Node left;
-    Node right;
-    Node(int data){
-        this.data = data;
-        left=null;
-        right=null;
-    }
-}
-
-
-class GfG {
-    
-    static Node buildTree(String str){
-        
-        if(str.length()==0 || str.charAt(0)=='N'){
-            return null;
-        }
-        
-        String ip[] = str.split(" ");
-        // Create the root of the tree
-        Node root = new Node(Integer.parseInt(ip[0]));
-        // Push the root to the queue
-        
-        Queue<Node> queue = new LinkedList<>(); 
-        
-        queue.add(root);
-        // Starting from the second element
-        
-        int i = 1;
-        while(queue.size()>0 && i < ip.length) {
-            
-            // Get and remove the front of the queue
-            Node currNode = queue.peek();
-            queue.remove();
-                
-            // Get the current node's value from the string
-            String currVal = ip[i];
-                
-            // If the left child is not null
-            if(!currVal.equals("N")) {
-                    
-                // Create the left child for the current node
-                currNode.left = new Node(Integer.parseInt(currVal));
-                // Push it to the queue
-                queue.add(currNode.left);
-            }
-                
-            // For the right child
-            i++;
-            if(i >= ip.length)
-                break;
-                
-            currVal = ip[i];
-                
-            // If the right child is not null
-            if(!currVal.equals("N")) {
-                    
-                // Create the right child for the current node
-                currNode.right = new Node(Integer.parseInt(currVal));
-                    
-                // Push it to the queue
-                queue.add(currNode.right);
-            }
-            i++;
-        }
-        
-        return root;
-    }
-    void inOrder(Node node) {
-        if (node == null) {
-            return;
-        }
- 
-        inOrder(node.left);
-        System.out.print(node.data + " ");
- 
-        inOrder(node.right);
-    }
-    
-	public static void main (String[] args) throws IOException{
-	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	        
-	        int t=Integer.parseInt(br.readLine());
-    
-	        while(t-- > 0){
-	            String s = br.readLine();
-    	    	Node root = buildTree(s);
-			    Solution g=new Solution();
-                System.out.println(g.isSumProperty(root));
-    	        
-	        
-System.out.println("~");
-}
-	}
-}
-
-
-// } Driver Code Ends
 
 class Solution{
-    boolean flag = true;
-    public int isSumProperty(Node root){
-        f(root);
-        
-        if(flag){
-            return 1;
-        }
-        else{
-            return 0;
-        }
+    public static int isSumProperty(Node root){
+        boolean x = f(root);
+        if(x) return 1;
+        return 0;
     }
-    void f(Node root){
-        if(root == null || (root.left==null && root.right==null) || !flag){
-            return;
+    static boolean f(Node root){
+        if(root == null || (root.left==null && root.right==null)){
+            return true;
         }
         
         int sum = 0;
-        if(root.left != null){
-            sum += root.left.data;
-        }
-        if(root.right != null){
-            sum += root.right.data;
-        }
-        if(sum != root.data){
-            flag = false;
-        }
+        if(root.left != null) sum += root.left.data;
+        if(root.right != null) sum += root.right.data;
         
-        f(root.left);
-        f(root.right);
+        if(sum != root.data){
+            return false;
+        }
+        else{
+            return f(root.left) && f(root.right);
+        }
     }
 }
