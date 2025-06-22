3702b8a2c0ab4383cd82eef1ea090138c2672400
Nishant Kumar <NISHANTRAJENDRAKUMAR@GMAIL.COM>
2025-06-22 18:29:04 +0530

    Time: 1 ms (100%), Space: 44.7 MB (99.93%) - LeetHub

--- 3903-inverse-coin-change/3903-inverse-coin-change.java
+++ 3903-inverse-coin-change/3903-inverse-coin-change.java
@@ -1,29 +1,27 @@
-import java.util.*;
+class Solution{
+    public List<Integer> findCoins(int[] numWays){
+        int n = numWays.length;
+        int[] myWays = new int[n+1];
+        myWays[0] = 1;
 
-class Solution {
-    public List<Integer> findCoins(int[] numWays) {
-    int n = numWays.length;
-    long[] myWays = new long[n + 1];
-    myWays[0] = 1;
+        List<Integer> res = new ArrayList<>();
 
-    List<Integer> res = new ArrayList<>();
-
-    for (int i = 1; i <= n; i++) {
-        // If `myWays[x] == numWays[x]`, move on.
-        if (myWays[i] == numWays[i - 1]) continue;
-
-        // If `myWays[x] + 1 == numWays[x]` → add that value as a coin in our basket and update `myWays`, so `myWays[x...n]` accounts for ways with the new coin.
-        if (numWays[i - 1] - myWays[i] == 1) {
-            res.add(i);
-            for (int j = i; j <= n; j++) {
-                myWays[j] += myWays[j - i];
+        for(int amount=1; amount<=n; amount++){
+            if(myWays[amount] == numWays[amount-1]){
+                continue;
             }
+            else if(myWays[amount] + 1 == numWays[amount-1]){
+                res.add(amount); // coin == amount
 
-        // If `myWays[x] + 1 < numWays[x]` → no solution. (*see below for why)
-        } else {
-            return Collections.emptyList();
-        }
+                for(int nextAmount=amount; nextAmount<=n; nextAmount++){
+                    myWays[nextAmount] += myWays[nextAmount - amount];
+                }
+            }
+            else{
+                return Collections.emptyList();
+            }
+        }    
+
+        return res;
     }
-    return res;
-}
 }
