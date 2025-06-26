f3290f745fad4b15653b525fddc7b3da37f4456b
Nishant Kumar <NISHANTRAJENDRAKUMAR@GMAIL.COM>
2025-06-26 23:00:27 +0530

    Time: 21 ms (65.67%), Space: 42.4 MB (72.14%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.java
@@ -2,11 +2,11 @@ class Solution{
     int[] augCuts;
     int[][] memo;
     public int minCost(int n, int[] cuts){
-        augCuts = new int[cuts.length+2];
+        augCuts = new int[cuts.length + 2];
         for(int i=0; i<cuts.length; i++){
-            augCuts[i+1] = cuts[i];
+            augCuts[i] = cuts[i];
         }
-        augCuts[augCuts.length-1] = n;
+        augCuts[cuts.length] = n;
         Arrays.sort(augCuts);
 
         memo = new int[augCuts.length][augCuts.length];
@@ -16,23 +16,23 @@ class Solution{
 
         return f(0, augCuts.length-1);
     }
-    int f(int s, int e){
-        if(s+1 == e){ // can't be cut further
+    int f(int i, int j){
+        if(i+1 == j){
             return 0;
         }
-        if(memo[s][e]!=-1){
-            return memo[s][e];
+        if(memo[i][j] != -1){
+            return memo[i][j];
         }
 
         int best = Integer.MAX_VALUE;
-        for(int k=s+1; k<e; k++){ // deciding where to cut the rod
-            int cutCost = augCuts[e] - augCuts[s];
-            int leftCost = f(s, k);
-            int rightCost = f(k, e);
+        for(int k=i+1; k<j; k++){
+            int cutCost = augCuts[j] - augCuts[i];
+            int leftCost = f(i, k);
+            int rightCost = f(k, j);
 
-            best = Math.min(best, cutCost+leftCost+rightCost);
+            best = Math.min(best, cutCost + leftCost + rightCost);
         }
 
-        return memo[s][e] = best;
+        return memo[i][j] = best;
     }
 }
