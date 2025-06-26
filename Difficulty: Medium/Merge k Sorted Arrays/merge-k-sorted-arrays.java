ad3cd3beef8a2e0e938a067774a568cb48480964
Nishant Kumar <NISHANTRAJENDRAKUMAR@GMAIL.COM>
2025-06-27 01:15:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge k Sorted Arrays/merge-k-sorted-arrays.java
+++ Difficulty: Medium/Merge k Sorted Arrays/merge-k-sorted-arrays.java
@@ -1,32 +1,29 @@
 class Solution{
     class Pair{
-        int eleIndex;
-        int listIndex;
-        int num;
+        int num, li, ei;
         
-        public Pair(int eleIndex, int listIndex, int num){
-            this.eleIndex = eleIndex;
-            this.listIndex = listIndex;
+        public Pair(int num, int li, int ei){
             this.num = num;
+            this.li = li;
+            this.ei = ei;
         }
     }
-    public ArrayList<Integer> mergeKArrays(int[][] arr, int k){
-        PriorityQueue<Pair> q = new PriorityQueue<>((a, b) ->{
-            return a.num - b.num;
-        });
-        
-        for(int i=0; i<k; i++){
-            q.add(new Pair(0, i, arr[i][0]));
-        }
-        
+    public ArrayList<Integer> mergeKArrays(int[][] g, int k){
+        PriorityQueue<Pair> q = new PriorityQueue<>((a, b)-> a.num - b.num);
         ArrayList<Integer> l = new ArrayList<>();
+        int m = g.length, n = g[0].length;
+        
+        for(int i=0; i<m; i++){
+            q.add(new Pair(g[i][0], i, 0));
+        }
         
         while(!q.isEmpty()){
             Pair poll = q.poll();
             l.add(poll.num);
+            int li = poll.li, ei = poll.ei;
             
-            if(poll.eleIndex < k-1){
-                q.add(new Pair(poll.eleIndex+1, poll.listIndex, arr[poll.listIndex][poll.eleIndex+1]));
+            if(ei < n-1){
+                q.add(new Pair(g[li][ei + 1], li, ei + 1));
             }
         }
         
