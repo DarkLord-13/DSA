545e6acd1ac1c34009d7760ca60cb9a0db60587a
Nishant Kumar <NISHANTRAJENDRAKUMAR@GMAIL.COM>
2025-06-27 09:48:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Median in a row-wise sorted Matrix/median-in-a-rowwise-sorted-matrix.java
+++ Difficulty: Hard/Median in a row-wise sorted Matrix/median-in-a-rowwise-sorted-matrix.java
@@ -1,77 +1,45 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while (t-- > 0) {
-            int R = Integer.parseInt(read.readLine());
-            int C = Integer.parseInt(read.readLine());
-            int matrix[][] = new int[R][C];
-            int c = 0;
-            for (int i = 0; i < R; i++) {
-                String line[] = read.readLine().trim().split(" ");
-                for (int j = 0; j < C; j++) {
-                    matrix[i][j] = Integer.parseInt(line[j]);
-                }
-            }
-            Solution ob = new Solution();
-            int ans = ob.median(matrix);
-            System.out.println(ans);
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
 class Solution{
-    int median(int g[][]){
-        int m = g.length;
-        int n = g[0].length;
-        int left = (m*n)/2;
-        
-        int l = Integer.MAX_VALUE;
-        int h = Integer.MIN_VALUE;
-        int mid = 0;
+    int[][] g;
+    int m, n;
+    int median(int[][] gx){
+        g = gx;
+        m = g.length; n = g[0].length;
+        int l = g[0][0], h = g[m-1][n-1];
         
         for(int i=0; i<m; i++){
             l = Math.min(l, g[i][0]);
             h = Math.max(h, g[i][n-1]);
         }
         
+        int left = (m * n) >> 1;
+        
         while(l <= h){
-            mid = (l+h) >> 1;
+            int mid = (l + h) >> 1;
             
             int lse = 0;
             for(int i=0; i<m; i++){
-                lse += lessThanMid(g[i], mid);
+                lse += f(i, mid);
             }
-
-            if(lse <= left){
-                l = mid + 1;
-            }
-            else{
+            
+            if(lse > left){
                 h = mid - 1;
             }
-        }
-        
-        return l - 1;
-    }
-    int lessThanMid(int[] arr, int mid){
-        
-        int count = 0;
-        for(int i: arr){
-            if(i < mid){
-                count++;
+            else{
+                l = mid + 1;
             }
         }
         
-        return count;
+        return l;
+    }
+    int f(int row, int num){
+        
+        int c = 0;
+        for(int j=0; j<n; j++){
+            if(g[row][j] <= num){
+                c++;
+            }
+        }
+        
+        return c;
     }
 }
