f24628f4326c512704bbb4a996545b5469b3450e
Nishant Kumar <NISHANTRAJENDRAKUMAR@GMAIL.COM>
2025-06-27 18:10:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Inversions/count-inversions.java
+++ Difficulty: Medium/Count Inversions/count-inversions.java
@@ -1,44 +1,48 @@
 class Solution{
-    static int inversionCount(int[] arr){
-        return mergeSortAndCount(0, arr.length - 1, arr);
+    static int[] arr;
+    static int inversionCount(int[] arrx){
+        arr = arrx;
+        
+        return merge(0, arr.length - 1);
     }
-    static private int mergeSortAndCount(int l, int r, int[] arr){
+    static int merge(int l, int r){
         if(l >= r){
             return 0;
         }
         
-        int m = (l + r) >> 1;
+        int mid = (l + r) >> 1;
         
         int count = 0;
         
-        count += mergeSortAndCount(l, m, arr);
-        count += mergeSortAndCount(m+1, r, arr);
-        count += mergeAndCount(l, m, r, arr);
+        count += merge(l, mid);
+        count += merge(mid+1, r);
+        count += mergeSort(l, mid, r);
         
         return count;
     }
-    static private int mergeAndCount(int l, int m, int r, int[] arr){
-        int n1 = m-l+1, n2 = r-m;
-        int[] a = Arrays.copyOfRange(arr, l, m+1), b = Arrays.copyOfRange(arr, m+1, r+1);
-        int count = 0;
+    static int mergeSort(int l, int mid, int r){
+        int[] arr1 = Arrays.copyOfRange(arr, l, mid+1);
+        int[] arr2 = Arrays.copyOfRange(arr, mid+1, r+1);
+        int n1 = arr1.length, n2 = arr2.length;
         
-        int i = 0, j = 0, k = l;
+        int i = 0, j = 0, k = l, count = 0;
+        
         while(i < n1 && j < n2){
-            if(a[i] <= b[j]){
-                arr[k++] = a[i++];
+            if(arr1[i] <= arr2[j]){
+                arr[k++] = arr1[i++];
             }
             else{
-                arr[k++] = b[j++];
                 count += (n1 - i);
+                arr[k++] = arr2[j++];
             }
         }
         
         while(i < n1){
-            arr[k++] = a[i++];
+            arr[k++] = arr1[i++];
         }
         
         while(j < n2){
-            arr[k++] = b[j++];
+            arr[k++] = arr2[j++];
         }
         
         return count;
