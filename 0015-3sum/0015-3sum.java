ab567435566cc9ee8ee17fecc9b62bb7f24b8ef7
Nishant Kumar <NISHANTRAJENDRAKUMAR@GMAIL.COM>
2025-06-30 00:53:49 +0530

    Time: 28 ms (80.6%), Space: 51.6 MB (48.94%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,24 +1,42 @@
 class Solution{
     public List<List<Integer>> threeSum(int[] a){
-        Arrays.sort(a); int n = a.length;
+        Arrays.sort(a);
+        int n = a.length;
         List<List<Integer>> ll = new ArrayList<>();
 
-        int i=0;
-        for(i=0; i<n-2; i++){
-            
-            int l=i+1, r=n-1;
-            while(l<r){
-                int sum = a[i] + a[l] + a[r];
-                if(sum>0) r--;
-                else if(sum<0) l++;
-                else{
-                    ll.add(Arrays.asList(a[i], a[l], a[r]));
+        for(int i=0; i<n-2; i++){
+            if(i > 0 && a[i] == a[i-1]){
+                continue;
+            }
 
-                    while(l<r && a[l]==a[l+1]) l++; l++;
-                    while(r>l && a[r]==a[r-1]) r--; r--;
-                    while(i<n-2 && a[i]==a[i+1]) i++; 
-                }                
-            }            
+            int l = i+1, r = n-1;
+
+            while(l < r){              
+                int sum = a[i] + a[l] + a[r];
+                if(sum < 0){
+                    l++;
+                }
+                else if(sum > 0){
+                    r--;
+                }
+                else{
+                    List<Integer> list = new ArrayList<>();
+                    list.add(a[i]);
+                    list.add(a[l]);
+                    list.add(a[r]);
+                    ll.add(new ArrayList<>(list));
+
+                    while(l < r && a[l] == a[l+1]){
+                        l++;
+                    }
+                    while(l < r && a[r] == a[r-1]){
+                        r--;
+                    }
+
+                    l++;
+                    r--;
+                }
+            }
         }
 
         return ll;
