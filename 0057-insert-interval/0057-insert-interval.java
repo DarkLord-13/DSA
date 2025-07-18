d712cd91c73814a676f29c43541c2ddc8670dc45
Nishant Kumar <NISHANTRAJENDRAKUMAR@GMAIL.COM>
2025-07-18 18:32:36 +0530

    Time: 2 ms (31.75%), Space: 45.1 MB (26.07%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,37 +1,54 @@
 class Solution{
-    public int[][] insert(int[][] intervals, int[] newInterval){
-        ArrayList<int[]> l = new ArrayList<>();
-        int i=0, n=intervals.length;
-        // left side no overlapping
-        while(i<n && intervals[i][1]<newInterval[0]){
-            l.add(intervals[i]);
-            i++;
+    public int[][] insert(int[][] intervals, int[] newi){
+        List<List<Integer>> allIntervals = new ArrayList<>();
+        boolean added = false;
+
+        if(intervals.length == 0){
+            return new int[][] {newi};
+        }
+        
+        for(int[] i: intervals){
+            if(i[0] >= newi[0] && !added){
+                allIntervals.add(Arrays.asList(newi[0], newi[1]));
+                added = true;
+            }
+
+            allIntervals.add(Arrays.asList(i[0], i[1]));            
+        }
+        if(!added){
+            allIntervals.add(Arrays.asList(newi[0], newi[1]));
+        }
+        
+
+        List<List<Integer>> mergedIntervals = new ArrayList<>();
+        int len = allIntervals.size();
+
+        int i = 1;
+        int s = allIntervals.get(0).get(0);
+        int e = allIntervals.get(0).get(1);
+
+        while(i < len){
+
+            int maxEnd = e;
+            while(i < len && allIntervals.get(i).get(0) <= maxEnd){
+                maxEnd = Math.max(maxEnd, allIntervals.get(i).get(1));
+                i++;
+            }
+
+            mergedIntervals.add(Arrays.asList(s, maxEnd));
+
+            if(i < len){
+                s = allIntervals.get(i).get(0);
+                e = allIntervals.get(i).get(1);
+            }
         }
 
-        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
-        // overlapping middle
-        while(i<n && intervals[i][0]<=newInterval[1]){
-            min = Math.min(intervals[i][0], min);
-            max = Math.max(intervals[i][1], max);
-            i++;
-        }
-        min = Math.min(min, newInterval[0]);
-        max = Math.max(max, newInterval[1]);
-
-        int[] insert = new int[2];
-        insert[0] = min; insert[1] = max;
-        l.add(insert);
-
-        while(i<n){
-            l.add(intervals[i]);
-            i++;
+        int[][] res = new int[mergedIntervals.size()][2];
+        for(i=0; i<res.length; i++){
+            res[i][0] = mergedIntervals.get(i).get(0);
+            res[i][1] = mergedIntervals.get(i).get(1);
         }
 
-        int[][] ret = new int[l.size()][2];
-        for(i=0; i<l.size(); i++){
-            ret[i] = l.get(i);
-        }
-
-        return ret;
+        return res;
     }
 }
