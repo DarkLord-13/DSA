aeed5db10c0d0bcd1d8f5259d147aa8b69e39647
Nishant Kumar <NISHANTRAJENDRAKUMAR@GMAIL.COM>
2025-07-20 13:24:44 +0530

    Time: 79 ms (74.25%), Space: 73.1 MB (83.8%) - LeetHub

--- 0854-making-a-large-island/0854-making-a-large-island.java
+++ 0854-making-a-large-island/0854-making-a-large-island.java
@@ -1,124 +1,103 @@
 class Solution{
-    int[] parent;
-    int[] s;
-    int find(int x){
-        if(parent[x]!=x){
-            parent[x] = find(parent[x]);
-        }
-
-        return parent[x];
-    }
     void union(int x, int y){
         x = find(x);
         y = find(y);
 
-        if(x==y){
+        if(x == y){
             return;
-        }
+        }        
 
-        if(s[x]>s[y]){
-            parent[y] = x;
+        if(s[x] > s[y]){
+            p[y] = x;
             s[x] += s[y];
         }
         else{
-            parent[x] = y;
+            p[x] = y;
             s[y] += s[x];
         }
     }
+    int find(int x){
+        if(p[x] != x){
+            return p[x] = find(p[x]);
+        }
 
-    int[][] v;
-    int[][] g;
-    int[] x = {-1, 0, +1, 0};
-    int[] y = {0, +1, 0, -1};
+        return p[x];
+    }
+    int[] p, s;
     int n;
-    public int largestIsland(int[][] gx){
-        g = gx;
+    int[][] g, v;
+    int[] x = {-1, 0, +1, 0}, y = {0, +1, 0, -1};
+    public int largestIsland(int[][] g){
+        this.g = g;
         n = g.length;
-        parent = new int[n*n];
+        v = new int[n][n];
+        p = new int[n*n];
         s = new int[n*n];
-        for(int i=0; i<n*n; i++){
-            parent[i] = i;
-        }
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(g[i][j]==1){
-                    s[i*n + j] = 1;
-                }
-                else{
-                    s[i*n + j] = 0;
-                }
-            }
-        }
-        v = new int[n][n];        
 
+        for(int i=0; i<n*n; i++){
+            p[i] = i;
+        }
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                if(v[i][j]==0 && g[i][j]==1){
-                    dfs(i, j);
+                if(g[i][j] == 1){
+                    s[j + n*i] = 1;
                 }
             }
         }
 
-        for(int i=0; i<n*n; i++){
-            parent[i] = find(parent[i]);
-        } 
-        // print(s);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(v[i][j] == 0 && g[i][j] == 1){
+                    dfs(i, j, i, j);
+                }
+            }
+        }
 
         int max = 0;
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                max = Math.max(max, s[i*n + j]);
-                if(g[i][j]==0){
-                    int sum = 0;
+
+                if(g[i][j] == 0){
+                    int c = 0;
                     Set<Integer> set = new HashSet<>();
 
                     for(int p=0; p<4; p++){
-                        int ni = i+x[p];
-                        int nj = j+y[p];                        
+                        int ni = i + x[p], nj = j + y[p];
 
-                        if(isSafe(ni, nj)){
-                            int up = parent[ni*n + nj];
+                        if(ni >= 0 && ni < n && nj >= 0 && nj < n && g[ni][nj] == 1){
+                            int parentNode = find(nj + n * ni);
 
-                            if(!set.contains(up)){
-                                sum += s[up];
-                                set.add(up);
-                            }                        
+                            if(!set.contains(parentNode)){
+                                set.add(parentNode);
+                                c += s[parentNode];
+                            }
                         }
                     }
 
-                    max = Math.max(max, sum+1);
+                    max = Math.max(max, c + 1);
                 }
             }
         }
 
-        return max;
+        return max == 0 ? n*n : max;
     }
-    void dfs(int i, int j){
+    void dfs(int i, int j, int pi, int pj){
+        if(!isSafe(i, j)){
+            return;
+        }
+
         v[i][j] = 1;
+        union(j + n*i, pj + n * pi);
 
         for(int p=0; p<4; p++){
-            int ni = i + x[p];
-            int nj = j + y[p];
-
-            if(isSafe(ni, nj) && v[ni][nj]==0){
-                union(i*n + j, ni*n + nj);
-                dfs(ni, nj);
-            }
+            dfs(i+x[p], j+y[p], pi, pj);
         }
     }
     boolean isSafe(int i, int j){
-        if(i<0 || j<0 || i>=n || j>=n || g[i][j]==0){
+        if(i < 0 || j < 0 || i >= n || j >= n || v[i][j] == 1 || g[i][j] == 0){
             return false;
         }
-        else{
-            return true;
-        }
-    }
-    void print(int[] arr){
-        for(int i: arr){
-            System.out.print(i + " ");
-        }
-        System.out.println();
+
+        return true;
     }
 }
-
