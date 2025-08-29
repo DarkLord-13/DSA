41a2041cca2b7af1cfa83ef4531d58be478b44f9
Nishant Kumar <NISHANTRAJENDRAKUMAR@GMAIL.COM>
2025-08-30 01:06:16 +0530

    Time: 103 ms (1.92%), Space: 76.1 MB (7.26%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,28 +1,34 @@
 class Solution{
-    int[] p; int n;
-    int[][] memo;
-    public int maxProfit(int[] px){
-        p=px; n=p.length;
-        memo = new int[n][3]; 
-        for(int i=0; i<n; i++)
-            Arrays.fill(memo[i], -1);
+    private int[] prices;
+    private int n;
+    private Integer[][] memo;
+    public int maxProfit(int[] prices){
+        this.prices = prices;
+        this.n = prices.length;
+        this.memo = new Integer[n][3];
 
-        return f(0, 0);     
+        return f(0, 0);
     }
     int f(int i, int b){
-        if(i>=n) return 0;
-        if(memo[i][b]!=-1) 
+        if(i >= n){
+            return 0;
+        }
+        if(memo[i][b] != null){
             return memo[i][b];
-
-        int x=0, y=0, z=0;
-        if(b==0){
-            x = -p[i] + f(i+1, 1);
         }
-        else if(b==1){
-            y = p[i] + f(i+1, 2);            
-        }
-        z = f(i+1, b);
 
-        return memo[i][b] = Math.max(x, Math.max(y, Math.max(z, memo[i][b])));
+        int x = 0;
+        if(b == 0){
+            x = -prices[i] + f(i+1, 1);
+        }
+
+        int z = f(i+1, b);
+
+        int y = 0;
+        if(b == 1){
+            y = prices[i] + f(i+1, 2);
+        }
+
+        return memo[i][b] = Math.max(x, Math.max(y, z));
     }
 }
