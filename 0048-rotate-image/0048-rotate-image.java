9dcd630a45e803345a6a6c8ee471af9297777547
Nishant Kumar <NISHANTRAJENDRAKUMAR@GMAIL.COM>
2025-08-30 12:54:06 +0530

    Time: 0 ms (100%), Space: 42.3 MB (59.17%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,21 +1,33 @@
 class Solution{
     public void rotate(int[][] g){
-        int n=g.length;
+        int n = g.length;
 
         for(int i=0; i<n; i++){
-            for(int j=i; j<n; j++){
-                int temp = g[i][j];
-                g[i][j] = g[j][i];
-                g[j][i] = temp;
+            for(int j=i+1; j<n; j++){
+                inverse(i, j, g);
             }
         }
 
         for(int i=0; i<n; i++){
-            for(int j=0; j<n/2; j++){
-                int temp = g[i][j];
-                g[i][j] = g[i][n-j-1];
-                g[i][n-j-1] = temp;
+            int l = 0, h = n-1;
+
+            while(l < h){
+                swap(i, l, h, g);
+                l++;
+                h--;
             }
         }
+    }
+
+    private void swap(int i, int l, int h, int[][] g){
+        int temp = g[i][l];
+        g[i][l] = g[i][h];
+        g[i][h] = temp;
+    }
+
+    private void inverse(int i, int j, int[][] g){
+        int temp = g[i][j];
+        g[i][j] = g[j][i];
+        g[j][i] = temp;
     }
 }
