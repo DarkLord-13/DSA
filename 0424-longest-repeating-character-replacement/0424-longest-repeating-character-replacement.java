b3f8d75182817034a46a759576e107e3318287e1
Nishant Kumar <NISHANTRAJENDRAKUMAR@GMAIL.COM>
2025-09-03 07:44:36 +0530

    Time: 5 ms (99.47%), Space: 45 MB (40.77%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,29 +1,26 @@
 class Solution{
-    public int characterReplacement(String s, int k){
-        int l=0, r=0, n=s.length(), maxFreq=0, maxLen=0;
-        int[] arr = new int[26];
+    public int characterReplacement(String str, int k){
+        char[] s = str.toCharArray();
+        int l = 0, r = 0, maxLen = 0, n = s.length, maxFreq = 0;
+        int[] freq = new int[26];
 
-        while(r<n){
-            char c = s.charAt(r);
-            arr[c-'A']++;
-            int currLen = r-l+1;
-            maxFreq = Math.max(maxFreq, arr[c-'A']);
+        while(r < n){
+            char c = s[r];
+            freq[c - 'A']++;
+            int currLen = r - l + 1;
+            maxFreq = Math.max(maxFreq, freq[c - 'A']);
 
-            while(l<r && !valid(currLen, maxFreq, k)){
-                arr[s.charAt(l)-'A']--;
+            while(l < r && (currLen - maxFreq > k)){
+                freq[s[l] - 'A']--;
+                currLen--;
+                for(int f: freq) maxFreq = Math.max(maxFreq, f);
                 l++;
-                currLen = r-l+1;
-                for(int i: arr) maxFreq = Math.max(maxFreq, i);
-            }            
-            
-            maxLen = Math.max(maxLen, currLen);            
+            }
+
+            maxLen = Math.max(maxLen, currLen);
             r++;
         }
 
         return maxLen;
-    }
-    boolean valid(int currLen, int maxFreq, int k){
-        if(currLen-maxFreq > k) return false;
-        return true;
     }
 }
