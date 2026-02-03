3b237ecc2d26930d190c5241c79a9076252d69b2
Nishant Kumar <NISHANTRAJENDRAKUMAR@GMAIL.COM>
2026-02-04 02:00:58 +0530

    Time: 8 ms (53.72%), Space: 47.9 MB (14.72%) - LeetHub

--- 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.java
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.java
@@ -1,50 +1,23 @@
 class Solution{
-    private class Pair{
-        int b, i;
-
-        public Pair(int b, int i){
-            this.b = b;
-            this.i = i;
-        }
-    }
     public long minimumSteps(String s){
-        int n = s.length();
-        int[] nums = new int[n];
-        for(int i=0; i<n; i++){
-            nums[i] = s.charAt(i) - '0';
-        }
-
-        List<Long> og = f(nums);
-        Arrays.sort(nums);
-        List<Long> sorted = f(nums);
-
+        int l = 0, r = s.length() - 1;
+        char[] arr = s.toCharArray();
         long swaps = 0;
-        for(int i=0; i<og.size(); i++){
-            swaps += (sorted.get(i) - og.get(i));
+
+        while(l < r){
+            if(arr[l] == '1'){
+                if(arr[r] == '1'){
+                    r--;
+                }
+                else{
+                    swaps += (r - l);
+                    r--;
+                    l++;
+                }
+            }
+            else l++;
         }
 
-        return swaps;
-    }
-
-    private List<Long> f(int[] nums){
-        List<Long> oneIndex = new ArrayList<>();
-
-        for(int i=0; i<nums.length; i++){
-            if(nums[i] == 1) oneIndex.add(i * 1L);
-        }
-
-        return oneIndex;
+        return swaps;        
     }
 }
-/**
-101
-011
-
-100
-001
-
-0111
-
-011010
-000111   3
- */
