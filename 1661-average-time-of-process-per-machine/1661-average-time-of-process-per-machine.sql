6a7effd87de3701fb834ac97d6b0f9b35281615b
Nishant Kumar <NISHANTRAJENDRAKUMAR@GMAIL.COM>
2026-02-24 23:45:49 +0530

    Time: 239 ms (61.75%), Space: 0B (100%) - LeetHub

---
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -0,0 +1,8 @@
+select a.machine_id, round(avg(b.timestamp - a.timestamp), 3) as processing_time
+from Activity a
+join Activity b
+on a.machine_id = b.machine_id and 
+a.process_id = b.process_id and 
+a.activity_type != b.activity_type 
+where a.activity_type like 'start'
+group by a.machine_id
