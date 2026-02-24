1853bf4a781f3ced060431399b4d0f5bb2cc53f2
Nishant Kumar <NISHANTRAJENDRAKUMAR@GMAIL.COM>
2026-02-25 01:04:43 +0530

    Time: 618 ms (72.61%), Space: 0B (100%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,6 @@
+select s.user_id, 
+    round(sum(if(action like 'confirmed', 1, 0)) / sum(if(action like 'confirmed', 1, 1)), 2) as confirmation_rate
+from Signups s
+left join Confirmations c
+on s.user_id = c.user_id
+group by user_id
